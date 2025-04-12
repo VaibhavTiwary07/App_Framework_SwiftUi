b285cf5486e310faae5b66be3f79f8f6cd42f067
vticr7 <vti9227@gmail.com>
2025-04-12 20:24:24 +0530

    Initial Commit

---
+++ App-Framework_SwiftUi/App_Framework_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_Framework_SwiftUiApp.swift
+//  App-Framework_SwiftUi
+//
+//  Created by Vaibhav  Tiwary on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct App_Framework_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App-Framework_SwiftUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App-Framework_SwiftUi
+//
+//  Created by Vaibhav  Tiwary on 12/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
